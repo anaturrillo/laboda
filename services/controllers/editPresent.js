95b56ae1084b57a0e0418b8786a67e8353577ae8
ana <anaturrillo@gmail.com>
2017-01-23 13:56:01 -0300

    editar regalo

--- services/controllers/editPresent.js
+++ services/controllers/editPresent.js
@@ -1,5 +1,9 @@
 const queryPromise = require('../../lib/queryPromise');
 
 module.exports = function (connection, modification) {
+  const qp = queryPromise(connection);
 
+  const post = qp(modification);
+
+  return post('UPDATE availablePresents SET ?')
 };
